f1ccee70a8df17eefc15dfb5aeae9aa3301f7726
Mikhail Saveliev <zdarovayrodi@icloud.com>
2023-12-17 18:23:27 +0300

    refactor: add service, repository & swagger to builder & app

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,10 +1,35 @@
+using Microsoft.OpenApi.Models;
+using TodoList.Repositories;
+using TodoList.Services.Tasks;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
+
+    // services
+    builder.Services.AddScoped<ITaskService, TaskService>();
+    builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+
+    // controllers
+    builder.Services.AddControllers();
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todolist API", Version = "v1" });
+    });
 }
+
+
 
 var app = builder.Build();
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todolist API");
+    });
+    
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
