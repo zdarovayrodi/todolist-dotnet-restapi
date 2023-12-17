1a74f03aef5dd13eb43a4763c0c6dd81c3cda74e
Mikhail Saveliev <zdarovayrodi@icloud.com>
2023-12-17 18:19:20 +0300

    refactor: add setter for Task.Id & empty constructor

--- TodoList/Models/Task.cs
+++ TodoList/Models/Task.cs
@@ -2,13 +2,17 @@ namespace TodoList.Models;
 
 public class Task
 {
-    public Guid Id { get; }
+    public Guid Id { get; set; }
     public string Title { get; }
     public string Description { get; }
     public DateTime Deadline { get; }
     public DateTime LastModifiedDateTime { get; }
     public List<string> Tags { get; }
-    
+
+    public Task()
+    {
+    }
+
     public Task(Guid id, string title, string description, DateTime deadline, DateTime lastModifiedDateTime, List<string> tags)
     {
         Id = id;
