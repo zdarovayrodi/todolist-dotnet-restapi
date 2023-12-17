6f013737817a17ac5dfa2d9abf0bd49819bb63db
Mikhail Saveliev <zdarovayrodi@icloud.com>
2023-12-17 18:22:48 +0300

    refactor: add get, post, put, delete logic

--- TodoList/Controllers/TasksController.cs
+++ TodoList/Controllers/TasksController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using TodoList.Contracts.TodoList;
+using TodoList.Services.Tasks;
+using Task = TodoList.Models.Task;
 
 namespace TodoList.Controllers;
 
@@ -7,27 +9,108 @@ namespace TodoList.Controllers;
 [Route("[controller]")]
 public class TasksController : ControllerBase
 {
+    private readonly ITaskService _taskService;
+
+    public TasksController(ITaskService taskService)
+    {
+        _taskService = taskService;
+    }
+
     [HttpPost]
     public IActionResult CreateTask(CreateTaskRequest request)
     {
-        return Ok();
+        var task = new Task(
+            Guid.NewGuid(),
+            request.Title,
+            request.Description,
+            request.Deadline,
+            DateTime.UtcNow,
+            request.Tags);
+    
+        _taskService.CreateTask(task);
+    
+        var response = new TaskResponse(
+            task.Id,
+            task.Title,
+            task.Description,
+            task.Deadline,
+            task.LastModifiedDateTime,
+            task.Tags);
+        
+        return CreatedAtAction(
+            actionName: nameof(GetTask),
+            routeValues: new {id = task.Id },
+            value: response);
     }
     
     [HttpGet("{id:guid}")]
     public IActionResult GetTask(Guid id)
     {
-        return Ok();
+        var task = _taskService.GetTask(id);
+        if (task is null) return NotFound();
+    
+        var response = new TaskResponse(
+            task.Id,
+            task.Title,
+            task.Description,
+            task.Deadline,
+            task.LastModifiedDateTime,
+            task.Tags);
+        
+        return Ok(response);
     }
     
     [HttpPut("{id:guid}")]
     public IActionResult PutTask(Guid id, UpsertTaskRequest request)
     {
-        return Ok();
+        var task = _taskService.GetTask(id);
+        if (task is null) return NotFound();
+    
+        var updatedTask = new Task(
+            task.Id,
+            request.Title,
+            request.Description,
+            request.Deadline,
+            DateTime.UtcNow,
+            request.Tags);
+    
+        _taskService.UpsertTask(updatedTask);
+    
+        var response = new TaskResponse(
+            updatedTask.Id,
+            updatedTask.Title,
+            updatedTask.Description,
+            updatedTask.Deadline,
+            updatedTask.LastModifiedDateTime,
+            updatedTask.Tags);
+        
+        return Ok(response);
     }
     
     [HttpDelete("{id:guid}")]
     public IActionResult DeleteTask(Guid id)
     {
-        return Ok();
+        var task = _taskService.GetTask(id);
+        if (task is null) return NotFound();
+    
+        _taskService.DeleteTask(id);
+    
+        return NoContent();
+    }
+    
+    [HttpGet]
+    public IActionResult GetTasks()
+    {
+        var tasks = _taskService.GetTasks();
+    
+        var response = tasks.Select(task => new TaskResponse(
+            task.Id,
+            task.Title,
+            task.Description,
+            task.Deadline,
+            task.LastModifiedDateTime,
+            task.Tags));
+        
+        return Ok(response);
     }
 }
